56f7c2c33862539ece9b7e27dc5fd1576d82f83c
SapeBiel <bielsanchezpey@alumnes.ilerna.com>
2026-02-24 16:43:29 +0100

    Creacion de classes

---
+++ src/el_juego_de_pingu/Casilla.java
@@ -0,0 +1,17 @@
+package el_juego_de_pingu;
+
+public class Casilla {
+	
+	protected int posicion;
+	protected String tipo;
+	
+	
+	/*
+	public void activarEfecto(jugador: Jugador) {
+		
+	}
+	*/
+	
+	
+	
+}
---
+++ src/el_juego_de_pingu/Tablero.java
@@ -0,0 +1,21 @@
+package el_juego_de_pingu;
+
+public class Tablero {
+
+	private int midaTablero;
+	
+	
+	public void generarTableroAleatorio() {
+		
+	}
+	
+	/*public Casilla getCasilla(int posicion) {
+		
+	}
+	
+	
+	public void moverJugador(Jugador jugador, int posicion) {
+		
+	}*/
+	
+}
