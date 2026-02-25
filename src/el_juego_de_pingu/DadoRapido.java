a46a0a2d603bdfbb0b91568e3dca8c06fe49fde6
SapeBiel <bielsanchezpey@alumnes.ilerna.com>
2026-02-25 17:13:07 +0100

    Dado Rapido tirar ()

--- src/el_juego_de_pingu/DadoRapido.java
+++ src/el_juego_de_pingu/DadoRapido.java
@@ -2,15 +2,22 @@ package el_juego_de_pingu;
 
 public class DadoRapido extends Dado{
 
-	/*
+	
 	public int tirar() {
 
+	int numero = (int)(Math.random() * (10 - 5 + 1)) + 5;
+	
+	return numero;
+
 	}
-	*/
+	
 
 	@Override
 	public void usar(Jugador jugador) {
 
+		this.tirar();
+		
+		
 	}
 	
 }
