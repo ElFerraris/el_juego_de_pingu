57fe7b1fd86b87047f9c683ab79fa802f83e0178
SapeBiel <bielsanchezpey@alumnes.ilerna.com>
2026-03-04 20:09:02 +0100

    Crear metodos de Juego

--- src/el_juego_de_pingu/CPU.java
+++ src/el_juego_de_pingu/CPU.java
@@ -1,12 +1,22 @@
 package el_juego_de_pingu;
 
-public class CPU extends Juego{
+public class CPU extends Jugador{
 
 	private String dificultad;
 	
+	public CPU(String id, String nombre, String dificultad) {
+        super(id, nombre); // Llama al constructor de Jugador
+        this.dificultad = dificultad;
+    }
 	
 	public void decidirAccion() {
 
+		if (getInventario().tieneObjeto("Dados")) {
+            getInventario().usarObjeto("Dados", this);
+        } else {
+            this.tirarDado();
+        }
+		
 	}
 
 	public void atacarJugador(Jugador jugador) {
