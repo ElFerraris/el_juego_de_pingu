c7182cd462b37a292b6e758704c3bb5d94f1b9aa
ElFerraris <galol.exe@alumnes.ilerna.com>
2026-03-23 23:58:08 +0100

    Vista y Controlador Login

---
+++ src/vista/Login.java
@@ -0,0 +1,17 @@
+package vista;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+	@FXML
+	private Button btn_login;
+	@FXML
+	private PasswordField text_pswd;
+	@FXML
+	private TextField text_user;
+	
+}
